63dc6018d076d93133baabcfbda01affbe2b43bc
HsuHsiaoHsuan <hsuhsiaohsuan+git@gmail.com>
2014-05-13 17:15:18 +0800

    backup

--- WorldBike/src/main/java/idv/funnybrain/bike/world/data/IParser.java
+++ WorldBike/src/main/java/idv/funnybrain/bike/world/data/IParser.java
@@ -1,7 +1,14 @@
 package idv.funnybrain.bike.world.data;
 
+import java.util.HashMap;
+
 /**
  * Created by Freeman on 2014/5/13.
  */
 public interface IParser {
+    public boolean hasValue();
+
+    public String getLastUpdate();
+
+    public HashMap<String, IStation> getStationList();
 }
