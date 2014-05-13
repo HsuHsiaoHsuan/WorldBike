b822e4afc69a1fba1c05c829db21615bb0f71766
HsuHsiaoHsuan <hsuhsiaohsuan+git@gmail.com>
2014-05-13 17:05:59 +0800

    backup

--- WorldBike/src/main/java/idv/funnybrain/bike/world/data/IStation.java
+++ WorldBike/src/main/java/idv/funnybrain/bike/world/data/IStation.java
@@ -1,7 +1,22 @@
 package idv.funnybrain.bike.world.data;
 
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * Created by Freeman on 2014/5/13.
  */
-public class IStation {
+public interface IStation {
+    public String getId();
+
+    public String getStationName();
+
+    public String toString();
+
+    public LatLng getLatLng();
+
+    public String getAddress();
+
+    public int getAvailableBikes();
+
+    public int getAvailableDocks();
 }
--- WorldBike/src/main/java/idv/funnybrain/bike/world/databases/IDBHelper.java
+++ WorldBike/src/main/java/idv/funnybrain/bike/world/databases/IDBHelper.java
@@ -1,7 +1,19 @@
 package idv.funnybrain.bike.world.databases;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+
 /**
  * Created by Freeman on 2014/5/13.
  */
 public interface IDBHelper {
+    public long insertFavor(ContentValues values);
+
+    public Cursor queryAll();
+
+    public boolean queryIsFavor(String id);
+
+    public int removeFavor(String id);
+
+    public void close();
 }
